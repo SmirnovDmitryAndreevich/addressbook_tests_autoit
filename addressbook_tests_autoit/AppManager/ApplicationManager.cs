e7fc52f10e22f6a10bf9ae405d61728757a6941b
Dmitry Smirnov <Dmitry.Smirnov@Emerson.com>
2022-02-07 15:05:15 +0300

    Add project files.

---
+++ addressbook_tests_autoit/AppManager/ApplicationManager.cs
@@ -0,0 +1,33 @@
+﻿using AutoItX3Lib;
+
+namespace addressbook_tests_autoit
+{
+    public class ApplicationManager
+    {
+        public static string WINTITLE = "Free Address Book";
+        private AutoItX3 aux;
+        private GroupHelper groupHelper;
+
+        public  ApplicationManager()
+        {
+            aux = new AutoItX3();
+            aux.Run(@"C:\Users\DSmirnov\Desktop\addressbook\AddressBook.exe", "", aux.SW_SHOWMAXIMIZED);
+            aux.WinWait(WINTITLE);
+            aux.WinActivate(WINTITLE);
+            aux.WinWaitActive(WINTITLE);
+            groupHelper = new GroupHelper(this);
+        }
+
+        public void Stop()
+        {
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d510");
+        }
+
+        public AutoItX3 Aux
+        {
+            get { return aux; }
+        }
+
+        public GroupHelper Groups { get { return groupHelper; } }
+    }
+}
---
+++ addressbook_tests_autoit/Tests/GroupRemovalTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoval()
+        {
+            int indexToRemove = 1;
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            app.Groups.AddGroupToRemove();
+
+            app.Groups.Remove(indexToRemove);
+            oldGroups.RemoveAt(indexToRemove-1);
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
